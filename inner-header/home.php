9ec8061f348cfb30c5ef776f7e7d7cfd9efa5b1a
richard-antenna <80421774+richard-antenna@users.noreply.github.com>
2023-11-06 12:23:12 -0500

    Update home.php

--- inner-header/home.php
+++ inner-header/home.php
@@ -1,14 +1,18 @@
 <?php 
-$bg_type = get_field("background_type");
 include ( MODULES . 'inner-header/image.php'); 
 ?>
 
-<section class="home-hero">
+<section class="home-hero mt12 bg-black pt10 pb10">
 	<div class="home-hero__content">
 		<div class="row row-site column">
 			<?php include ( MODULES . 'inner-header/label.php'); ?>
 			<?php include ( MODULES . 'inner-header/title.php'); ?>
 			<?php include ( MODULES . 'inner-header/intro-text.php'); ?>
+			
+			<figure>
+			<?php echo $inner_header_image;  ?>
+			</figure>
+			
 		</div>
 	</div>
 </section>
